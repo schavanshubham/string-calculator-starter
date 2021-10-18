be32ff40814a667fd361317fac9e0adf72b93d0b
Shubham suresh chavan <schavanshubham1997@gmail.com>
2021-10-18 11:38:21 +0530

    test6

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -38,5 +38,7 @@ class StringCalculatorShould {
    public void  negativeInputReturnsException() throws Exception {
    	stringCalculator.add("-1");
    }
-  
+   public void ignoreNumbersGreaterThan1000() throws Exception {
+	   	assertEquals(stringCalculator.add("10,10,1001"),20);
+	   }
 }
