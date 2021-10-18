2c60abb142513a57b7ace011eacaf4b2357f861a
Shubham suresh chavan <schavanshubham1997@gmail.com>
2021-10-18 11:37:46 +0530

    test5

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -34,5 +34,9 @@ class StringCalculatorShould {
    public void multipleDelimiterReturnSum() throws Exception {
 	   	assertEquals(stringCalculator.add("1;2"),3);
 	   }
+   @Test(expectedExceptions=Exception.class )
+   public void  negativeInputReturnsException() throws Exception {
+   	stringCalculator.add("-1");
+   }
   
 }
